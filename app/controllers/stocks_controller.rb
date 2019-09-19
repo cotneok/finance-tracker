1f2e407d8721503a01b2b4c8bed125270ef96ecc
Tsotne <cotne.ok@gmail.com>
2019-09-20 00:31:11 +0400

    Fixing errors and refractoring

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,19 +1,14 @@
 class StocksController < ApplicationController
 
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        respond_to do |format|
-         format.js { render partial: 'users/result' }
-        end
-     else
-       flash.now[:danger] = "You have entered an incorrect symbol"
-       render partial: 'users/result'
-     end
-    else
+    if params[:stock].blank?
       flash.now[:danger] = "You have entered an empty search string"
-      render partial: 'users/result'
+    else
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
+    end
+    respond_to do |format|
+      format.js { render partial: 'users/result' }
     end
   end
 end
