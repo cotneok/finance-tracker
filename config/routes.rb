7dfb5a50fa1038eecbeb097b0b046caa98fa1dc3
Tsotne <cotne.ok@gmail.com>
2019-09-22 11:52:28 +0400

    Add search bar to my friends page

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'my_friends', to: 'users#my_friends'
   resources :users, only: [:show]
   resources :friendships
+  get 'search_friends', to: 'users#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
